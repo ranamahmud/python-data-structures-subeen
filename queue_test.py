a55cb1a7dd9802abf613825035073f4bf198d8fd
Md Rana Mahmud <ranamd0@gmail.com>
2020-02-22 13:04:38 -0600

    Queue added

---
+++ queue_test.py
@@ -0,0 +1,20 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+    def enqueue(self, item):
+        self.items.append(item)
+    def dequeue(self):
+        return self.items.pop(0)
+    def is_empty(self):
+        if self.items == []:
+            return True
+        return False
+
+if __name__ == "__main__":
+    q = Queue()
+    q.enqueue("Tahmid")
+    q.enqueue("Rafi")
+    q.enqueue("Tamim")
+    while not q.is_empty():
+        person = q.dequeue()
+        print(person)
