d717a0acee9b98dd5aeb3acbc2ada5ca67d8f4c2
Md Rana Mahmud <ranamd0@gmail.com>
2020-02-24 08:14:58 -0600

    fibonacci3 explaining fibonacci added

---
+++ fibonacci3.py
@@ -0,0 +1,7 @@
+def fibonacci(n):
+    print("Trying to find fibonacci for", n)
+    if n == 1 or n == 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+if __name__ == "__main__":
+    print("5th fibonacci number is", fibonacci(5))
