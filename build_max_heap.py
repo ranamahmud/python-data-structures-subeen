afdcb23080a248de8a49c9629a7c3cc4be2d8657
Md Rana Mahmud <ranamd0@gmail.com>
2020-02-26 21:34:56 +0600

    print binary tree updated

--- build_max_heap.py
+++ build_max_heap.py
@@ -1,4 +1,5 @@
 from max_heapify import max_heapify
+from binarytree import build
 def build_max_heap(heap):
     heap_size = len(heap) - 1
     for i in range(heap_size // 2, 0, -1):
@@ -9,3 +10,13 @@ if __name__ == "__main__":
     print("Before build heap", heap)
     build_max_heap(heap)
     print("After building heap", heap)
+    
+    heap = [None, 12, 7, 1, 3, 10, 17, 19, 2, 5]
+    print("Before build heap")
+    root = build(heap[1:])
+    print(root)
+    build_max_heap(heap)
+    print("After building heap")
+    root= build(heap[1:])
+    print(root)
+    
